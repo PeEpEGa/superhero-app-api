b9f8644edea7ac46daad4305ea4800ff99b4d945
Vitalii Yaltukhov <vitalii3797@gmail.com>
2025-09-15 09:49:59 +0300

    test

--- api/index.ts
+++ api/index.ts
@@ -37,9 +37,12 @@ async function buildApp() {
   });
 
   await fastify.register(cors, {
-    origin: "*",
+    origin: (origin, cb) => {
+      cb(null, true);
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   });
 
   fastify.register(errorHandlerPlugin);
