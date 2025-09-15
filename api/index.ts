0de4271943d73c10a58d823370f18ef547a636e3
Vitalii Yaltukhov <vitalii3797@gmail.com>
2025-09-15 08:58:44 +0300

    preparation for deployment

--- api/index.ts
+++ api/index.ts
@@ -3,34 +3,59 @@ import errorHandlerPlugin from "../src/shared/plugins/errorHandler.plugin";
 import superPowerRoutes from "../src/features/super-power/superpower.route";
 import multipart from "@fastify/multipart";
 import superheroRoutes from "../src/features/superhero/superhero.route";
+import cors from "@fastify/cors";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
 
 async function start() {
   try {
     const fastify = Fastify({
-      logger: {
-        level: "info",
-        transport: {
-          target: "pino-pretty",
-          options: {
-            translateTime: "HH:MM:ss Z",
-            ignore: "pid,hostname",
-          },
-        },
-      },
-      //   logger: true,
+      // logger: {
+      //   level: "info",
+      //   transport: {
+      //     target: "pino-pretty",
+      //     options: {
+      //       translateTime: "HH:MM:ss Z",
+      //       ignore: "pid,hostname",
+      //     },
+      //   },
+      // },
+      logger: true,
+    });
+
+    await fastify.register(cors, {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
     });
 
     fastify.register(errorHandlerPlugin);
     await fastify.register(multipart, {
       limits: { fileSize: 5 * 1024 * 1024 },
-      // attachFieldsToBody: true,
     });
+
+    // Register Swagger
+    await fastify.register(swagger, {
+      openapi: {
+        info: {
+          title: "Music Tracks API",
+          description: "API for managing superheroes",
+          version: "1.0.0",
+        },
+      },
+    });
+
+    // Register Swagger UI
+    await fastify.register(swaggerUi, {
+      routePrefix: "/documentation",
+      uiConfig: {
+        docExpansion: "list",
+        deepLinking: true,
+      },
+    });
+
     fastify.register(superPowerRoutes, { prefix: "/api" });
     fastify.register(superheroRoutes, { prefix: "/api" });
-
-    fastify.get("/", async function handler(request, reply) {
-      return { hello: "world" };
-    });
 
     await fastify.listen({ port: 3000 });
   } catch (error) {
